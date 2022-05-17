e0324ea69e21efe50767fac061f01110cc762de8
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-05-17 19:26:10 +0300

    Fix formatting

--- Design-Patterns-Examples/SOLID Design Principles/DependencyInversion.cs
+++ Design-Patterns-Examples/SOLID Design Principles/DependencyInversion.cs
@@ -8,77 +8,77 @@
   // hl modules should not depend on low-level; both should depend on abstractions
   // abstractions should not depend on details; details should depend on abstractions
 	  public enum Relationship
+{
+	Parent,
+	Child,
+	Sibling
+}
+
+public class Person
+{
+	public string Name;
+	// public DateTime DateOfBirth;
+}
+
+public interface IRelationshipBrowser
+{
+	IEnumerable<Person> FindAllChildrenOf(string name);
+}
+
+public class Relationships : IRelationshipBrowser // low-level
+{
+	private List<(Person, Relationship, Person)> relations
+	  = new List<(Person, Relationship, Person)>();
+
+	public void AddParentAndChild(Person parent, Person child)
 	{
-		Parent,
-		Child,
-		Sibling
+		relations.Add((parent, Relationship.Parent, child));
+		relations.Add((child, Relationship.Child, parent));
 	}
-	
-	public class Person
+
+	public List<(Person, Relationship, Person)> Relations => relations;
+
+	public IEnumerable<Person> FindAllChildrenOf(string name)
 	{
-		public string Name;
-		// public DateTime DateOfBirth;
+		return relations
+		  .Where(x => x.Item1.Name == name
+					  && x.Item2 == Relationship.Parent).Select(r => r.Item3);
 	}
-	
-	public interface IRelationshipBrowser
+}
+
+public class Research
+{
+	public Research(Relationships relationships)
 	{
-		IEnumerable<Person> FindAllChildrenOf(string name);
+		// high-level: find all of john's children
+		//var relations = relationships.Relations;
+		//foreach (var r in relations
+		//  .Where(x => x.Item1.Name == "John"
+		//              && x.Item2 == Relationship.Parent))
+		//{
+		//  WriteLine($"John has a child called {r.Item3.Name}");
+		//}
 	}
-	
-	public class Relationships : IRelationshipBrowser // low-level
+
+	public Research(IRelationshipBrowser browser)
 	{
-		private List<(Person, Relationship, Person)> relations
-		  = new List<(Person, Relationship, Person)>();
-	
-		public void AddParentAndChild(Person parent, Person child)
+		foreach (var p in browser.FindAllChildrenOf("John"))
 		{
-			relations.Add((parent, Relationship.Parent, child));
-			relations.Add((child, Relationship.Child, parent));
-		}
-	
-		public List<(Person, Relationship, Person)> Relations => relations;
-	
-		public IEnumerable<Person> FindAllChildrenOf(string name)
-		{
-			return relations
-			  .Where(x => x.Item1.Name == name
-						  && x.Item2 == Relationship.Parent).Select(r => r.Item3);
-		}
-	}
-	
-	public class Research
-	{
-		public Research(Relationships relationships)
-		{
-			// high-level: find all of john's children
-			//var relations = relationships.Relations;
-			//foreach (var r in relations
-			//  .Where(x => x.Item1.Name == "John"
-			//              && x.Item2 == Relationship.Parent))
-			//{
-			//  WriteLine($"John has a child called {r.Item3.Name}");
-			//}
-		}
-	
-		public Research(IRelationshipBrowser browser)
-		{
-			foreach (var p in browser.FindAllChildrenOf("John"))
-			{
-				WriteLine($"John has a child called {p.Name}");
-			}
-		}
-	
-		static void Main(string[] args)
-		{
-			var parent = new Person { Name = "John" };
-			var child1 = new Person { Name = "Chris" };
-			var child2 = new Person { Name = "Matt" };
-	
-			// low-level module
-			var relationships = new Relationships();
-			relationships.AddParentAndChild(parent, child1);
-			relationships.AddParentAndChild(parent, child2);
-			new Research(relationships);
+			WriteLine($"John has a child called {p.Name}");
 		}
 	}
+
+	static void Main(string[] args)
+	{
+		var parent = new Person { Name = "John" };
+		var child1 = new Person { Name = "Chris" };
+		var child2 = new Person { Name = "Matt" };
+
+		// low-level module
+		var relationships = new Relationships();
+		relationships.AddParentAndChild(parent, child1);
+		relationships.AddParentAndChild(parent, child2);
+		new Research(relationships);
+	}
+}
 }
--- Design-Patterns-Examples/SOLID Design Principles/InterfaceSegregation.cs
+++ Design-Patterns-Examples/SOLID Design Principles/InterfaceSegregation.cs
@@ -4,117 +4,117 @@ namespace Design-Patterns-Examples.SOLID Design Principles
 	using System;
 
 	public class Document
+{
+}
+
+public interface IMachine
+{
+	void Print(Document d);
+	void Fax(Document d);
+	void Scan(Document d);
+}
+
+// ok if you need a multifunction machine
+public class MultiFunctionPrinter : IMachine
+{
+	public void Print(Document d)
 	{
+		//
 	}
 
-	public interface IMachine
+	public void Fax(Document d)
 	{
-		void Print(Document d);
-		void Fax(Document d);
-		void Scan(Document d);
+		//
 	}
 
-	// ok if you need a multifunction machine
-	public class MultiFunctionPrinter : IMachine
+	public void Scan(Document d)
 	{
-		public void Print(Document d)
-		{
-			//
-		}
+		//
+	}
+}
 
-		public void Fax(Document d)
-		{
-			//
-		}
-
-		public void Scan(Document d)
-		{
-			//
-		}
+public class OldFashionedPrinter : IMachine
+{
+	public void Print(Document d)
+	{
+		// yep
 	}
 
-	public class OldFashionedPrinter : IMachine
+	public void Fax(Document d)
 	{
-		public void Print(Document d)
-		{
-			// yep
-		}
-
-		public void Fax(Document d)
-		{
-			throw new System.NotImplementedException();
-		}
-
-		public void Scan(Document d)
-		{
-			throw new System.NotImplementedException();
-		}
+		throw new System.NotImplementedException();
 	}
 
-	public interface IPrinter
+	public void Scan(Document d)
 	{
-		void Print(Document d);
+		throw new System.NotImplementedException();
 	}
+}
 
-	public interface IScanner
-	{
-		void Scan(Document d);
-	}
+public interface IPrinter
+{
+	void Print(Document d);
+}
 
-	public class Printer : IPrinter
-	{
-		public void Print(Document d)
-		{
+public interface IScanner
+{
+	void Scan(Document d);
+}
 
-		}
-	}
-
-	public class Photocopier : IPrinter, IScanner
-	{
-		public void Print(Document d)
-		{
-			throw new System.NotImplementedException();
-		}
-
-		public void Scan(Document d)
-		{
-			throw new System.NotImplementedException();
-		}
-	}
-
-	public interface IMultiFunctionDevice : IPrinter, IScanner //
+public class Printer : IPrinter
+{
+	public void Print(Document d)
 	{
 
 	}
+}
 
-	public struct MultiFunctionMachine : IMultiFunctionDevice
+public class Photocopier : IPrinter, IScanner
+{
+	public void Print(Document d)
 	{
-		// compose this out of several modules
-		private IPrinter printer;
-		private IScanner scanner;
-
-		public MultiFunctionMachine(IPrinter printer, IScanner scanner)
-		{
-			if (printer == null)
-			{
-				throw new ArgumentNullException(paramName: nameof(printer));
-			}
-			if (scanner == null)
-			{
-				throw new ArgumentNullException(paramName: nameof(scanner));
-			}
-			this.printer = printer;
-			this.scanner = scanner;
-		}
-
-		public void Print(Document d)
-		{
-			printer.Print(d);
-		}
-
-		public void Scan(Document d)
-		{
-			scanner.Scan(d);
-		}
+		throw new System.NotImplementedException();
 	}
+
+	public void Scan(Document d)
+	{
+		throw new System.NotImplementedException();
+	}
+}
+
+public interface IMultiFunctionDevice : IPrinter, IScanner //
+{
+
+}
+
+public struct MultiFunctionMachine : IMultiFunctionDevice
+{
+	// compose this out of several modules
+	private IPrinter printer;
+	private IScanner scanner;
+
+	public MultiFunctionMachine(IPrinter printer, IScanner scanner)
+	{
+		if (printer == null)
+		{
+			throw new ArgumentNullException(paramName: nameof(printer));
+		}
+		if (scanner == null)
+		{
+			throw new ArgumentNullException(paramName: nameof(scanner));
+		}
+		this.printer = printer;
+		this.scanner = scanner;
+	}
+
+	public void Print(Document d)
+	{
+		printer.Print(d);
+	}
+
+	public void Scan(Document d)
+	{
+		scanner.Scan(d);
+	}
+}
 }
