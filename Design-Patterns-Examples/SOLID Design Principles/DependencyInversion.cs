7cec81bc05754dfe0da50a4d42405f62a0c5052f
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-05-20 18:01:54 +0300

    Refactor, add docs

--- Design-Patterns-Examples/SOLID Design Principles/DependencyInversion.cs
+++ Design-Patterns-Examples/SOLID Design Principles/DependencyInversion.cs
@@ -5,80 +5,80 @@
 	using System.Linq;
 	using static System.Console;
 
-  // hl modules should not depend on low-level; both should depend on abstractions
-  // abstractions should not depend on details; details should depend on abstractions
-	  public enum Relationship
-{
-	Parent,
-	Child,
-	Sibling
-}
-
-public class Person
-{
-	public string Name;
-	// public DateTime DateOfBirth;
-}
-
-public interface IRelationshipBrowser
-{
-	IEnumerable<Person> FindAllChildrenOf(string name);
-}
-
-public class Relationships : IRelationshipBrowser // low-level
-{
-	private List<(Person, Relationship, Person)> relations
-	  = new List<(Person, Relationship, Person)>();
-
-	public void AddParentAndChild(Person parent, Person child)
+	// High level modules should not depend on low-level; both should depend on abstractions.
+	// Abstractions should not depend on details. Details should depend on abstractions.
+	public enum Relationship
 	{
-		relations.Add((parent, Relationship.Parent, child));
-		relations.Add((child, Relationship.Child, parent));
+		Parent,
+		Child,
+		Sibling
 	}
-
-	public List<(Person, Relationship, Person)> Relations => relations;
-
-	public IEnumerable<Person> FindAllChildrenOf(string name)
+	
+	public class Person
 	{
-		return relations
-		  .Where(x => x.Item1.Name == name
-					  && x.Item2 == Relationship.Parent).Select(r => r.Item3);
+		public string Name;
+		// public DateTime DateOfBirth;
 	}
-}
-
-public class Research
-{
-	public Research(Relationships relationships)
+	
+	public interface IRelationshipBrowser
 	{
-		// high-level: find all of john's children
-		//var relations = relationships.Relations;
-		//foreach (var r in relations
-		//  .Where(x => x.Item1.Name == "John"
-		//              && x.Item2 == Relationship.Parent))
-		//{
-		//  WriteLine($"John has a child called {r.Item3.Name}");
-		//}
+		IEnumerable<Person> FindAllChildrenOf(string name);
 	}
-
-	public Research(IRelationshipBrowser browser)
+	
+	public class Relationships : IRelationshipBrowser // low-level
 	{
-		foreach (var p in browser.FindAllChildrenOf("John"))
+		private List<(Person, Relationship, Person)> relations
+		  = new List<(Person, Relationship, Person)>();
+	
+		public void AddParentAndChild(Person parent, Person child)
 		{
-			WriteLine($"John has a child called {p.Name}");
+			relations.Add((parent, Relationship.Parent, child));
+			relations.Add((child, Relationship.Child, parent));
+		}
+	
+		public List<(Person, Relationship, Person)> Relations => relations;
+	
+		public IEnumerable<Person> FindAllChildrenOf(string name)
+		{
+			return relations
+			  .Where(x => x.Item1.Name == name
+						  && x.Item2 == Relationship.Parent).Select(r => r.Item3);
 		}
 	}
-
-	static void Main(string[] args)
+	
+	public class Research
 	{
-		var parent = new Person { Name = "John" };
-		var child1 = new Person { Name = "Chris" };
-		var child2 = new Person { Name = "Matt" };
-
-		// low-level module
-		var relationships = new Relationships();
-		relationships.AddParentAndChild(parent, child1);
-		relationships.AddParentAndChild(parent, child2);
-		new Research(relationships);
+		public Research(Relationships relationships)
+		{
+			// high-level: find all of john's children
+			//var relations = relationships.Relations;
+			//foreach (var r in relations
+			//  .Where(x => x.Item1.Name == "John"
+			//              && x.Item2 == Relationship.Parent))
+			//{
+			//  WriteLine($"John has a child called {r.Item3.Name}");
+			//}
+		}
+	
+		public Research(IRelationshipBrowser browser)
+		{
+			foreach (var p in browser.FindAllChildrenOf("John"))
+			{
+				WriteLine($"John has a child called {p.Name}");
+			}
+		}
+	
+		static void Main(string[] args)
+		{
+			var parent = new Person { Name = "John" };
+			var child1 = new Person { Name = "Chris" };
+			var child2 = new Person { Name = "Matt" };
+	
+			// low-level module
+			var relationships = new Relationships();
+			relationships.AddParentAndChild(parent, child1);
+			relationships.AddParentAndChild(parent, child2);
+			new Research(relationships);
+		}
 	}
-}
 }
