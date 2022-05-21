7cdc26ca06f692c27dc0b47e1b71a2efed496d51
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-05-21 17:47:27 +0300

    Add docs and different simplier example

--- Design-Patterns-Examples/SOLID Design Principles/DependencyInversion.cs
+++ Design-Patterns-Examples/SOLID Design Principles/DependencyInversion.cs
@@ -5,88 +5,66 @@
 	using System.Linq;
 	using static System.Console;
 
-	// High level modules should not depend on low-level; both should depend on abstractions.
-	// Abstractions should not depend on details. Details should depend on abstractions.
-	public enum Relationship
-	{
-		Parent,
-		Child,
-		Sibling
-	}
-	
-	public class Person
-	{
-		public string Name;
-		// public DateTime DateOfBirth;
-	}
-	
-	// Low-level parts of the system are defined as Relationships
-	public interface IRelationshipBrowser
-	{
-		IEnumerable<Person> FindAllChildrenOf(string name);
-	}
-	
+        // Not following the Dependency Inversion Principle  
 
-	public class Relationships : IRelationshipBrowser // low-level
-	{
-	// A list of tuples, You can access the tuple by Item1, Item2, Item3... etc
-	// If you are not familiar with tuples, you can view the following official documentation here:
-	// https://docs.microsoft.com/en-us/dotnet/api/system.tuple-4?view=net-6.0
-	private List<(Person, Relationship, Person)> relations
-		  = new List<(Person, Relationship, Person)>();
-		
-		// Mimic of an API, not typical database operation
-		public void AddParentAndChild(Person parent, Person child)
-		{
-			relations.Add((parent, Relationship.Parent, child));
-			// Reverse relationship
-			relations.Add((child, Relationship.Child, parent));
-		}
-		
-		// Here we expose the Relations with public field
-		public List<(Person, Relationship, Person)> Relations => relations;
-	
-		public IEnumerable<Person> FindAllChildrenOf(string name)
-		{
-			return relations
-			  .Where(x => x.Item1.Name == name
-						  && x.Item2 == Relationship.Parent).Select(r => r.Item3);
-		}
-	}
-	
-	public class Research
-	{
-		public Research(Relationships relationships)
-		{
-			// high-level: find all of john's children
-			// var relations = relationships.Relations;
-			// foreach (var r in relations
-			// .Where(x => x.Item1.Name == "John"
-			//              && x.Item2 == Relationship.Parent))
-			// {
-			//  WriteLine($"John has a child called {r.Item3.Name}");
-			// }
-		}
-	
-		public Research(IRelationshipBrowser browser)
-		{
-			foreach (var p in browser.FindAllChildrenOf("John"))
-			{
-				WriteLine($"John has a child called {p.Name}");
-			}
-		}
-	
-		static void Main(string[] args)
-		{
-			var parent = new Person { Name = "John" };
-			var child1 = new Person { Name = "Chris" };
-			var child2 = new Person { Name = "Matt" };
-	
-			// low-level module
-			var relationships = new Relationships();
-			relationships.AddParentAndChild(parent, child1);
-			relationships.AddParentAndChild(parent, child2);
-			new Research(relationships);
-		}
-	}
+        // The salary calculator will calculate the salary of a employee by given hoursWorked and hourlyRate.
+        class SalaryCalculator
+        {
+            public float CalculateSalary(int hoursWorked, float hourlyRate) => hoursWorked * hourlyRate;
+        }
+
+        // The employee class will have a property called GetSalary executing CalculateSalary property in SalaryCalculator.
+        // These classes do not follow the “Dependency Inversion Principle” as the higher-level
+        // class EmployeeDetails is directly depending upon the lower level SalaryCalculator class.
+        public class EmployeeDetails
+        {
+            public int HoursWorked { get; set; }
+            public int HourlyRate { get; set; }
+            public float GetSalary()
+            {
+                var salaryCalculator = new SalaryCalculator();
+                return salaryCalculator.CalculateSalary(HoursWorked, HourlyRate);
+            }
+        }
+
+
+    // Following the Dependency Inversion Principle  
+
+    // Let's Create an interface ISalaryCalculator and then we have a class called SalaryCalculatorModified
+    // that implements this interface
+    public interface ISalaryCalculator
+    {
+        float CalculateSalary(int hoursWorked, float hourlyRate);
+    }
+    
+    public class SalaryCalculatorModified : ISalaryCalculator
+    {
+        public float CalculateSalary(int hoursWorked, float hourlyRate) => hoursWorked * hourlyRate;
+    }
+    
+    public class EmployeeDetailsModified
+    {
+    
+        // We are using dependancy injection to provive _salaryCalculator with data
+        private readonly ISalaryCalculator _salaryCalculator;
+    
+        public int HoursWorked { get; set; }
+        public int HourlyRate { get; set; }
+    
+        public EmployeeDetailsModified(ISalaryCalculator salaryCalculator)
+        {
+            _salaryCalculator = salaryCalculator;
+        }
+
+        // In the higher-level class EmployeeDetailsModified, we only depend upon the ISalaryCalculator interface
+        // and not the concrete class SalaryCalculator. Hence, when we create the EmployeeDetailsModified class, we specify
+        // the abstraction implementation to use.
+        public float GetSalary()
+        {
+        // In addition to this, the details of the CalculateSalary function are hidden from the EmployeeDetailsModified
+        // class and any changes to this function will not affect the interface being used.
+        return _salaryCalculator.CalculateSalary(HoursWorked, HourlyRate);
+        }
+    }
+
 }
