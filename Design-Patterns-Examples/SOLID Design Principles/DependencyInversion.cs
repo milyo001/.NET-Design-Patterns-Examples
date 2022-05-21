5a9d5754fe4bfca5d411ee9598f3735b115c00f6
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-05-21 17:27:06 +0300

    Add docs

--- Design-Patterns-Examples/SOLID Design Principles/DependencyInversion.cs
+++ Design-Patterns-Examples/SOLID Design Principles/DependencyInversion.cs
@@ -20,22 +20,30 @@
 		// public DateTime DateOfBirth;
 	}
 	
+	// Low-level parts of the system are defined as Relationships
 	public interface IRelationshipBrowser
 	{
 		IEnumerable<Person> FindAllChildrenOf(string name);
 	}
 	
+
 	public class Relationships : IRelationshipBrowser // low-level
 	{
-		private List<(Person, Relationship, Person)> relations
+	// A list of tuples, You can access the tuple by Item1, Item2, Item3... etc
+	// If you are not familiar with tuples, you can view the following official documentation here:
+	// https://docs.microsoft.com/en-us/dotnet/api/system.tuple-4?view=net-6.0
+	private List<(Person, Relationship, Person)> relations
 		  = new List<(Person, Relationship, Person)>();
-	
+		
+		// Mimic of an API, not typical database operation
 		public void AddParentAndChild(Person parent, Person child)
 		{
 			relations.Add((parent, Relationship.Parent, child));
+			// Reverse relationship
 			relations.Add((child, Relationship.Child, parent));
 		}
-	
+		
+		// Here we expose the Relations with public field
 		public List<(Person, Relationship, Person)> Relations => relations;
 	
 		public IEnumerable<Person> FindAllChildrenOf(string name)
@@ -51,13 +59,13 @@
 		public Research(Relationships relationships)
 		{
 			// high-level: find all of john's children
-			//var relations = relationships.Relations;
-			//foreach (var r in relations
-			//  .Where(x => x.Item1.Name == "John"
+			// var relations = relationships.Relations;
+			// foreach (var r in relations
+			// .Where(x => x.Item1.Name == "John"
 			//              && x.Item2 == Relationship.Parent))
-			//{
+			// {
 			//  WriteLine($"John has a child called {r.Item3.Name}");
-			//}
+			// }
 		}
 	
 		public Research(IRelationshipBrowser browser)
