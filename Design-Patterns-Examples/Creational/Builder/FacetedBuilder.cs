fae73d5236f0315f158cee87c59dbae0637d7de4
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-05-25 19:09:50 +0300

    Add more docs and extra example

--- Design-Patterns-Examples/Creational/Builder/FacetedBuilder.cs
+++ Design-Patterns-Examples/Creational/Builder/FacetedBuilder.cs
@@ -21,12 +21,17 @@ namespace DotNetDesignPatternDemos.Creational.BuilderFacets
         }
     }
 
-    public class PersonBuilder // This is not the ordinary builder it is a Facade
+    // This is not the ordinary builder, it is a Facade for accessing other builders
+    // You can check the Facade design pattern in the same repo
+    public class PersonBuilder
     {
-        // The object we're going to build
-        protected Person person = new Person(); // Note: This is a reference!
+        // This property is reference to the person that is being built up. Inaccessible due to protected specifier
+        protected Person person = new Person(); 
 
+        // Here we have the first builder which will build the address of a person
         public PersonAddressBuilder Lives => new PersonAddressBuilder(person);
+
+        // And then the second builder which will build the employment of a person
         public PersonJobBuilder Works => new PersonJobBuilder(person);
 
         public static implicit operator Person(PersonBuilder pb)
@@ -35,6 +40,7 @@ namespace DotNetDesignPatternDemos.Creational.BuilderFacets
         }
     }
 
+    // Designed to build up the address of a person
     public class PersonJobBuilder : PersonBuilder
     {
         public PersonJobBuilder(Person person)
@@ -45,18 +51,21 @@ namespace DotNetDesignPatternDemos.Creational.BuilderFacets
         public PersonJobBuilder At(string companyName)
         {
             person.CompanyName = companyName;
+            // Again we are returning the builder to allow chaining
             return this;
         }
 
         public PersonJobBuilder AsA(string position)
         {
             person.Position = position;
+            // Also set the person's position and return the builder
             return this;
         }
 
         public PersonJobBuilder Earning(int annualIncome)
         {
             person.AnnualIncome = annualIncome;
+            // Lastly set the person's annual income and return the builder
             return this;
         }
     }
@@ -94,17 +103,30 @@ namespace DotNetDesignPatternDemos.Creational.BuilderFacets
         static void Main(string[] args)
         {
             var pb = new PersonBuilder();
+            
             Person person = pb
-              .Lives
+              .Lives // Note that you can access the .At(), .In() and .WithPostcode() methods of the PersonAddressBuilder after you chain .Lives() first
                 .At("123 London Road")
-                .In("London")
-                .WithPostcode("SW12BC")
-              .Works
-                .At("Fabrikam")
-                .AsA("Engineer")
+                .In("Hamburg, Germany")
+                .WithPostcode("23311")
+              .Works // Note that you can access the .At and .AsA methods of the PersonJobBuilder after you chain .Works() first
+                .At("Microhard Corp.")
+                .AsA("Junior Developer")
                 .Earning(123000);
+            
+            // Example two
+            var person2 = pb
+                .Lives
+                .In("Sofia")
+                .At("Schtuttgart Strasse 99")
+                .Works
+                .Earning(50000)
+                .At("Edeka")
+                .AsA("Mrejar");
+
 
             WriteLine(person);
+            WriteLine(person2);
         }
     }
 }
