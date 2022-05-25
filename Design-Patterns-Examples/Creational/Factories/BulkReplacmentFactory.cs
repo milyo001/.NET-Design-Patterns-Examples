7f24cfd1898a9dae879c115479e53be24da98095
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-05-25 23:46:04 +0300

    Add docs for both examples

--- Design-Patterns-Examples/Creational/Factories/BulkReplacmentFactory.cs
+++ Design-Patterns-Examples/Creational/Factories/BulkReplacmentFactory.cs
@@ -2,26 +2,32 @@
 
 namespace BulkReplacement
 {
+    // A simple interface with two properties
     public interface ITheme
     {
         string TextColor { get; }
         string BgrColor { get; }
     }
 
+    // A simple class that implements the ITheme interface
     class LightTheme : ITheme
     {
         public string TextColor => "black";
         public string BgrColor => "white";
     }
 
+    // A simple class that implements the ITheme interface with reversed properties values
     class DarkTheme : ITheme
     {
         public string TextColor => "white";
         public string BgrColor => "dark gray";
     }
 
+    // Here is the factory class that creates the themes
     public class TrackingThemeFactory
     {
+        // When you use a weak reference, the application can still obtain a strong reference to the object,
+        // which prevents it from being collected. Store all themes in a weak reference dictionary.
         private readonly List<WeakReference<ITheme>> themes = new();
 
         public ITheme CreateTheme(bool dark)
@@ -31,6 +37,7 @@ namespace BulkReplacement
             return theme;
         }
 
+        // Returns a string  of all themes
         public string Info
         {
             get
@@ -52,6 +59,7 @@ namespace BulkReplacement
 
     public class ReplaceableThemeFactory
     {
+        // Slightly different from the previous one
         private readonly List<WeakReference<Ref<ITheme>>> themes
           = new();
 
@@ -63,14 +71,19 @@ namespace BulkReplacement
         public Ref<ITheme> CreateTheme(bool dark)
         {
             var r = new Ref<ITheme>(createThemeImpl(dark));
+            // Add wrapper, using shorthand new() syntax
             themes.Add(new(r));
             return r;
         }
 
+        // Bulk replacement method, replace all themes in the List collection with a given value
         public void ReplaceTheme(bool dark)
         {
+            // Loop through all WeakReferences
             foreach (var wr in themes)
             {
+                // Tries to get the reference in the List collection
+                // Out keyword is telling our compiler that we are passing parameter by reference
                 if (wr.TryGetTarget(out var reference))
                 {
                     reference.Value = createThemeImpl(dark);
@@ -79,6 +92,7 @@ namespace BulkReplacement
         }
     }
 
+    // This class works only with reference types only. This class can perform bulk replacement of themes.
     public class Ref<T> where T : class
     {
         public T Value;
@@ -97,16 +111,21 @@ namespace BulkReplacement
             var theme = factory.CreateTheme(true);
             var theme2 = factory.CreateTheme(false);
             Console.WriteLine(factory.Info);
+            // Output:
             // Dark theme
             // Light theme
 
 
-            // replacement
+            // Bulk replacement factory
             var factory2 = new ReplaceableThemeFactory();
             var magicTheme = factory2.CreateTheme(true);
-            Console.WriteLine(magicTheme.Value.BgrColor); // dark gray
+            Console.WriteLine(magicTheme.Value.BgrColor); 
             factory2.ReplaceTheme(false);
-            Console.WriteLine(magicTheme.Value.BgrColor); // white
+            Console.WriteLine(magicTheme.Value.BgrColor);
+            // Output of background color depending on the theme:
+            // Dark gray (See DarkTheme class)
+            // White (See LightTheme class)
+
         }
     }
 }
