c64bc1ac733f06d391fc6acb4fe09e5413785945
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-05-25 19:15:49 +0300

    Add summary .txt file

---
+++ Design-Patterns-Examples/Creational/Builder/Summary.cs
@@ -0,0 +1,5 @@
+﻿Summary for builder pattern: 
+* A builder is separate component for building an object
+* Can either give builder a constructor or return it via a static function
+* To make builder fluent (methods that can chain each other), return the builder itself (using "this" keyword)
+* Different facets of an object can be built with different builders working in tandem via base class
