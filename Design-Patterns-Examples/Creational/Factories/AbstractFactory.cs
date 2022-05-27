fed3c397b4b5f48116ef580b0b31efa5eb9f8a09
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-05-27 16:53:19 +0300

    Implement Main() to the example

--- Design-Patterns-Examples/Creational/Factories/AbstractFactory.cs
+++ Design-Patterns-Examples/Creational/Factories/AbstractFactory.cs
@@ -121,4 +121,26 @@
             return normalPhone.GetModelDetails();
         }
     }
+    class Program
+    {
+        // Test and debug here
+        static void Main()
+        {
+            IMobilePhone nokiaMobilePhone = new Nokia();
+            MobileClient nokiaClient = new MobileClient(nokiaMobilePhone);
+
+            Console.WriteLine("********* NOKIA **********");
+            Console.WriteLine(nokiaClient.GetSmartPhoneModelDetails());
+            Console.WriteLine(nokiaClient.GetNormalPhoneModelDetails());
+
+            IMobilePhone samsungMobilePhone = new Samsung();
+            MobileClient samsungClient = new MobileClient(samsungMobilePhone);
+
+            Console.WriteLine("******* SAMSUNG **********");
+            Console.WriteLine(samsungClient.GetSmartPhoneModelDetails());
+            Console.WriteLine(samsungClient.GetNormalPhoneModelDetails());
+
+            Console.ReadKey();
+        }
+    }
 }
