7a9187d7f1d145e7dfe4368b82439cc0548f191d
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-05-29 00:31:07 +0300

    Add docs

--- Design-Patterns-Examples/Creational/Prototype/PrototypeInheritance.cs
+++ Design-Patterns-Examples/Creational/Prototype/PrototypeInheritance.cs
@@ -1,9 +1,11 @@
-﻿namespace DotNetDesignPatternDemos.Creational.Prototype.Inheritance
+﻿namespace DotNetDesignPatternDemos.Creational.PrototypeInheritance
 {
     public interface IDeepCopyable<T> where T : new()
     {
         void CopyTo(T target);
 
+        // Explicitly casts an variable to IDeepCopyable<T>, there is no need to implement this missing memeber in classes 
+        // which inherit the interface, because we already have it here in the interface.
         public T DeepCopy()
         {
             T t = new T();
@@ -17,22 +19,27 @@
         public string StreetName;
         public int HouseNumber;
 
+        // Default constructor
+        public Address()
+        {
+
+        }        
+        
         public Address(string streetName, int houseNumber)
         {
             StreetName = streetName;
             HouseNumber = houseNumber;
         }
 
-        public Address()
-        {
-
-        }
+        
 
         public override string ToString()
         {
             return $"{nameof(StreetName)}: {StreetName}, {nameof(HouseNumber)}: {HouseNumber}";
         }
 
+        // Inherirted from IDeepCopyable, Note that there is no need to implment DeepCopy() in classes which inherit the interface
+        // The address CopyTo() is easy to implement because we have just two properties
         public void CopyTo(Address target)
         {
             target.StreetName = StreetName;
@@ -63,9 +70,17 @@
             return $"{nameof(Names)}: {string.Join(",", Names)}, {nameof(Address)}: {Address}";
         }
 
+        // Inherirted from IDeepCopyable, Note that there is no need to implment DeepCopy() in classes which inherit the interface
         public virtual void CopyTo(Person target)
         {
+            // The Array.Clone() method returns a new array (a shallow copy) object containing all the elements in the original array. 
+            //  A shallow copy means the contents (each array element) contains references to the same object as the elements in the original array.
+            // Don't forget to cast it to string[] because the Array.Clone() returns an object.
             target.Names = (string[])Names.Clone();
+
+            // And then deep copy the address
+            //  A deep copy (which neither of these methods performs) would create a new instance of each element's object,
+            //  resulting in a different, yet identical object.
             target.Address = Address.DeepCopy();
         }
     }
@@ -74,6 +89,7 @@
     {
         public int Salary;
 
+        // Inherirted from IDeepCopyable, Note that there is no need to implment DeepCopy() in classes which inherit the interface
         public void CopyTo(Employee target)
         {
             base.CopyTo(target);
@@ -86,6 +102,7 @@
         }
     }
 
+    // A static class which extends the IDeepCopyable<T>
     public static class DeepCopyExtensions
     {
         public static T DeepCopy<T>(this IDeepCopyable<T> item)
