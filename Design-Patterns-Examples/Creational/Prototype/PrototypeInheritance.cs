07e8bcda06df22bf7d19d612592738909ca88e2b
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-05-29 01:24:39 +0300

    Finish the documentation foe the example

--- Design-Patterns-Examples/Creational/Prototype/PrototypeInheritance.cs
+++ Design-Patterns-Examples/Creational/Prototype/PrototypeInheritance.cs
@@ -81,6 +81,9 @@
             // And then deep copy the address
             //  A deep copy (which neither of these methods performs) would create a new instance of each element's object,
             //  resulting in a different, yet identical object.
+
+            // With the help of the extentions methods we are calling the default Interface implementation: IDeepCopyable<T>.DeepCopy()
+            // This operation won't work without the extensions methods
             target.Address = Address.DeepCopy();
         }
     }
@@ -92,7 +95,9 @@
         // Inherirted from IDeepCopyable, Note that there is no need to implment DeepCopy() in classes which inherit the interface
         public void CopyTo(Employee target)
         {
+            // Calling CopyTo() from Person class
             base.CopyTo(target);
+            // And then setting the salary
             target.Salary = Salary;
         }
 
@@ -105,15 +110,22 @@
     // A static class which extends the IDeepCopyable<T>
     public static class DeepCopyExtensions
     {
+        // An extension for deep copy method, note that there is a constraint "where T : new()"
+        // because the interface use it 
         public static T DeepCopy<T>(this IDeepCopyable<T> item)
           where T : new()
         {
+            // It might seem confusing but we are executing the IDeepCopyable<T> method with the default
+            // implementation of DeepCopy() (see IDeepCopyable<T> interface at the beggining of the solution)
             return item.DeepCopy();
         }
 
+        // Unfortunately the new() constraint is everywhere, because it is defined in the interface
         public static T DeepCopy<T>(this T person)
           where T : Person, new()
         {
+            // Cast the person parameter from Person type to IDeepCopyable, so we can get the default implementation of IDeepCopyable<T>
+            // not the Person.DeepCopy()
             return ((IDeepCopyable<T>)person).DeepCopy();
         }
     }
@@ -126,8 +138,9 @@
             john.Names = new[] { "John", "Doe" };
             john.Address = new Address { HouseNumber = 123, StreetName = "London Road" };
             john.Salary = 321000;
+            
             var copy = john.DeepCopy();
-
+            
             copy.Names[1] = "Smith";
             copy.Address.HouseNumber++;
             copy.Salary = 123000;
