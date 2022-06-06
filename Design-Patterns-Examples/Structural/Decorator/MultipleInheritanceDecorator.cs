8df9260a7cec4ba53eddcecdca0d6bb348015e57
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-06-06 19:03:16 +0300

    Add example

---
+++ Design-Patterns-Examples/Structural/Decorator/MultipleInheritanceDecorator.cs
@@ -0,0 +1,41 @@
+﻿
+namespace DotNetDesignPatternDemos.Structural.Decorator
+{
+    public class Bird
+    {
+        public void Fly()
+        {
+
+        }
+    }
+
+    public class Lizard
+    {
+        public void Crawl()
+        {
+
+        }
+    }
+
+    public class Dragon // no multiple inheritance
+    {
+        private Bird bird;
+        private Lizard lizard;
+
+        public Dragon(Bird bird, Lizard lizard)
+        {
+            this.bird = bird ?? throw new ArgumentNullException(paramName: nameof(bird));
+            this.lizard = lizard ?? throw new ArgumentNullException(paramName: nameof(lizard));
+        }
+
+        public void Crawl()
+        {
+            lizard.Crawl();
+        }
+
+        public void Fly()
+        {
+            bird.Fly();
+        }
+    }
+}
