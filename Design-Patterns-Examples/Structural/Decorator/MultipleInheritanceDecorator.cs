d9caf0ad3ae3c31608f71b7f45f7b84493243992
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-06-06 19:29:17 +0300

    Finish example

--- Design-Patterns-Examples/Structural/Decorator/MultipleInheritanceDecorator.cs
+++ Design-Patterns-Examples/Structural/Decorator/MultipleInheritanceDecorator.cs
@@ -1,26 +1,43 @@
 ﻿
 namespace DotNetDesignPatternDemos.Structural.Decorator
 {
-    public class Bird
+    public interface IBird
     {
+        void Fly();
+    }
+
+    public interface ILizard
+    {
+        void Crawl();
+    }
+    
+    public class Bird : IBird
+    {
+        // Let's have a property weight here 
+        public int Weight { get; set; }
         public void Fly()
         {
-
+            Console.WriteLine($"Flying high in the sky with weight: {Weight}");
         }
     }
 
-    public class Lizard
+    public class Lizard : ILizard
     {
+        // Also here
+        public int Weight { get; set; }
+        
         public void Crawl()
         {
-
+            Console.WriteLine($"Crawling with a steady pace with weight: {Weight}");
         }
     }
 
-    public class Dragon // no multiple inheritance
+    public class Dragon : IBird, ILizard
     {
         private Bird bird;
         private Lizard lizard;
+        private int weight;
+
 
         public Dragon(Bird bird, Lizard lizard)
         {
@@ -36,6 +53,32 @@ namespace DotNetDesignPatternDemos.Structural.Decorator
         public void Fly()
         {
             bird.Fly();
+        }
+
+        // This is how you set the weight of the dragon, bird and lizard
+        public int Weight 
+        {
+            get => weight;
+            set
+            {
+                weight = value;
+                bird.Weight = value;
+                lizard.Weight = value;
+            }
+        }
+    }
+
+    public class Demo
+    {
+        static void Main(string[] args)
+        {
+            var drake = new Dragon(new Bird(), new Lizard());
+            drake.Weight = 1233;
+            // Will also set the Bird and Lizard weight property 
+            Console.WriteLine($"The dragon weight is: {drake.Weight}");
+            drake.Fly();
+            drake.Crawl();
+
         }
     }
 }
