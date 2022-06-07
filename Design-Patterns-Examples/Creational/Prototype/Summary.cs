42febeeeb5b5ac23ab3611eaa93d0c3e7823be8e
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-06-07 15:43:22 +0300

    Remove unused C# class summary.cs

--- Design-Patterns-Examples/Creational/Prototype/Summary.cs
+++
@@ -1,6 +0,0 @@
-﻿Summary for Prototype pattern:
-* To Implement a prototype partially construct an object and store it somewhere.
-* Clone the prototype and use it to construct another object.
-	* Implement your own deep copy functionality
-	* Serilize and deserialize
-* Customize the resulting instance
