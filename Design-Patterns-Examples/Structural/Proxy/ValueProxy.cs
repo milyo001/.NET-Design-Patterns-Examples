3a653bd5bd3a2fc027e8ee29be22fa2c20ffe657
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-06-08 17:26:21 +0300

    Add docs

--- Design-Patterns-Examples/Structural/Proxy/ValueProxy.cs
+++ Design-Patterns-Examples/Structural/Proxy/ValueProxy.cs
@@ -1,6 +1,6 @@
 ﻿using System.Diagnostics;
 
-// A proxy which is a wrapper around the primitive type
+// A proxy which is a stongly typed wrapper around the primitive type
 namespace DotNetDesignPatternDemos.Structural.Proxy.ValueProxy
 {
     [DebuggerDisplay("{value*100.0f}%")]
@@ -18,11 +18,15 @@ namespace DotNetDesignPatternDemos.Structural.Proxy.ValueProxy
         //      return new Percentage(value);
         //    }
 
+
+        // Let's define our operators
+        // Multiplication operator for the object
         public static float operator *(float f, Percentage p)
         {
             return f * p.value;
         }
 
+        // Then the plus operator for the object
         public static Percentage operator +(Percentage a, Percentage b)
         {
             return new Percentage(a.value + b.value);
@@ -33,22 +37,26 @@ namespace DotNetDesignPatternDemos.Structural.Proxy.ValueProxy
             return value.Percent();
         }
 
+        // Generated from Resharper
         public bool Equals(Percentage other)
         {
             return value.Equals(other.value);
         }
 
+        // Generated from Resharper
         public override bool Equals(object obj)
         {
             if (ReferenceEquals(null, obj)) return false;
             return obj is Percentage other && Equals(other);
         }
-
+        
+        // Generated from Resharper
         public override int GetHashCode()
         {
             return value.GetHashCode();
         }
 
+        // Once you use Console.WriteLine it will call this overriden method
         public override string ToString()
         {
             return $"{value * 100}%";
@@ -73,8 +81,8 @@ namespace DotNetDesignPatternDemos.Structural.Proxy.ValueProxy
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine(10f * 5.Percent());
-            Console.WriteLine(2.Percent() + 3.Percent());
+            Console.WriteLine(10f * 5.Percent()); // 0.5
+            Console.WriteLine(2.Percent() + 3.Percent()); // 5%
         }
     }
 }
