f6ab71cba06a9858ef982e7c869d0834e263efe8
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-06-16 21:17:57 +0300

    Add event observer example

---
+++ Design-Patterns-Examples/Behavior/Observer/EventObserver.cs
@@ -0,0 +1,39 @@
+﻿
+// Let's have a Person class that can fall ill and find a way to notify us that the person is ill and call the doctor
+namespace DotNetDesignPatternDemos.Behavioral.Observer
+{
+    public class FallsIllEventArgs
+    {
+        public string Address;
+    }
+
+    public class Person
+    {
+        public void CatchACold()
+        {
+            FallsIll?.Invoke(this,
+              new FallsIllEventArgs { Address = "123 London Road" });
+        }
+
+        // Using the event keyword
+        public event EventHandler<FallsIllEventArgs> FallsIll;
+    }
+
+    public class Demo
+    {
+        static void Main()
+        {
+            var person = new Person();
+
+            person.FallsIll += CallDoctor;
+
+            person.CatchACold();
+        }
+
+        // When person is ill call the doctor to help him 
+        private static void CallDoctor(object sender, FallsIllEventArgs eventArgs)
+        {
+            Console.WriteLine($"A doctor has been called to {eventArgs.Address}");
+        }
+    }
+}
