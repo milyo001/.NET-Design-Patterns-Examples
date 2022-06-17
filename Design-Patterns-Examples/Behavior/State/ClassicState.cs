29f1e30cefe35dd58de7ec47a0f737608f7206e0
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-06-17 22:18:11 +0300

    Add classic state example and docs

--- Design-Patterns-Examples/Behavior/State/ClassicState.cs
+++ Design-Patterns-Examples/Behavior/State/ClassicState.cs
@@ -1,6 +1,8 @@
 ﻿
+// This is scenario is included, but this is not the way we build state machines nowdays
 namespace DotNetDesignPatternDemos.Behavioral.State.Classic
 {
+    
     public class Switch
     {
         public State State = new OffState();
@@ -10,11 +12,15 @@ namespace DotNetDesignPatternDemos.Behavioral.State.Classic
 
     public abstract class State
     {
+        // If we call lightswitch.On() and the state object will be OnState which has overriden Off method the On method is
+        //  inherited from this base class
         public virtual void On(Switch sw)
         {
             Console.WriteLine("Light is already on.");
         }
 
+        // And if we call lightswitch.Off() and the state object will be OffState which has overriden On method the Off method
+        // is inherited from this base class
         public virtual void Off(Switch sw)
         {
             Console.WriteLine("Light is already off.");
@@ -27,9 +33,11 @@ namespace DotNetDesignPatternDemos.Behavioral.State.Classic
         {
             Console.WriteLine("Light turned on.");
         }
+        // In the OnState class, we can turn the light off only 
         public override void Off(Switch sw)
         {
             Console.WriteLine("Turning light off...");
+            // Set the state as new OffState in the abstract Switch base class
             sw.State = new OffState();
         }
     }
@@ -41,9 +49,11 @@ namespace DotNetDesignPatternDemos.Behavioral.State.Classic
             Console.WriteLine("Light turned off.");
         }
 
+        // In the OffState class, we can turn the light on only
         public override void On(Switch sw)
         {
             Console.WriteLine("Turning light on...");
+            // Set the state as new OnState in the abstract Switch base class
             sw.State = new OnState();
         }
     }
@@ -52,7 +62,10 @@ namespace DotNetDesignPatternDemos.Behavioral.State.Classic
     {
         public static void Main(string[] args)
         {
-            // ???
+            var lightSwitch = new Switch();
+            lightSwitch.On();
+            lightSwitch.Off();
+            lightSwitch.Off();
         }
     }
 }
