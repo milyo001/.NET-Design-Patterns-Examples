d428e3db7635433870fa7946b52ca213fa9b9758
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-06-17 17:01:19 +0300

    Remove more linq namespace

--- Design-Patterns-Examples/Behavior/Observer/PropertyDependancies.cs
+++ Design-Patterns-Examples/Behavior/Observer/PropertyDependancies.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
 using JetBrains.Annotations;
-using MoreLinq;
 
 namespace DotNetDesignPatternDemos.Behavioral.Observer.PropertyDependencies
 {
