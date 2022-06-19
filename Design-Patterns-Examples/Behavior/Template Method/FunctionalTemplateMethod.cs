25aa2ff51699e688f37454fea00e103ccf79d6fe
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-06-19 19:51:55 +0300

    Add functional template method example

---
+++ Design-Patterns-Examples/Behavior/Template Method/FunctionalTemplateMethod.cs
@@ -0,0 +1,56 @@
+﻿namespace DotNetDesignPatternDemos.Behavioral.TemplateMethod
+{
+    using static System.Console;
+    
+    // Template without inheritance using void delegates and functions
+    public static class GameTemplate
+    {
+        public static void Run(
+          Action start,
+          Action takeTurn,
+          Func<bool> haveWinner,
+          Func<int> winningPlayer)
+        {
+            start();
+            while (!haveWinner())
+                takeTurn();
+            WriteLine($"Player {winningPlayer()} wins.");
+        }
+    }
+
+    public class Demo2
+    {
+        static void Main(string[] args)
+        {
+            // No classes declarations
+            var numberOfPlayers = 2;
+            int currentPlayer = 0;
+            int turn = 1, maxTurns = 10;
+
+            // Functions that we will run in the template
+            void Start()
+            {
+                WriteLine($"Starting a game of chess with {numberOfPlayers} players.");
+            }
+
+            bool HaveWinner()
+            {
+                return turn == maxTurns;
+            }
+
+            void TakeTurn()
+            {
+                WriteLine($"Turn {turn++} taken by player {currentPlayer}.");
+                currentPlayer = (currentPlayer + 1) % numberOfPlayers;
+            }
+
+            int WinningPlayer()
+            {
+                return currentPlayer;
+            }
+
+            // Run the template
+            GameTemplate.Run(Start, TakeTurn, HaveWinner, WinningPlayer);
+        }
+    }
+}
